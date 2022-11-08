dfdbb9ce7ce4aa957dae3bacd8253171c356caab
andres174 <zambrasda17@gmail.com>
2022-11-08 13:17:50 -0500

    division y multiplicacion

--- app/src/main/java/com/example/appsumaandroid/MainActivity.java
+++ app/src/main/java/com/example/appsumaandroid/MainActivity.java
@@ -34,4 +34,19 @@ public class MainActivity extends AppCompatActivity {
         res = Integer.parseInt(this.num1.getText().toString()) - Integer.parseInt(this.num2.getText().toString());
         this.resultado.setText("El resultado es: " + res);
     }
+    public void Multiplicar(View v){
+        int res;
+        res = Integer.parseInt(this.num1.getText().toString()) * Integer.parseInt(this.num2.getText().toString());
+        this.resultado.setText("El resultado es: " + res);
+    }
+    public void Dividir(View v){
+        float res;
+        if ((Integer.parseInt(this.num1.getText().toString())> 0) && (Integer.parseInt(this.num2.getText().toString())> 0) ){
+            res = Float.parseFloat(this.num1.getText().toString()) / Float.parseFloat(this.num2.getText().toString());
+            this.resultado.setText("El resultado es: " + res);
+        } else {
+            this.resultado.setText("No se puede realizar la división");
+        }
+
+    }
 }
